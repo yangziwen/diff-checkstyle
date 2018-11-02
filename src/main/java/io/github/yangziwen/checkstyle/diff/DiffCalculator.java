9e4e83a990156cd437769505360027b67d1ca2d3
yangziwen <yangziwen@baidu.com>
2018-11-02 08:40:11 +0800

    提取DiffHelper类

--- src/main/java/io/github/yangziwen/checkstyle/diff/DiffCalculator.java
+++ src/main/java/io/github/yangziwen/checkstyle/diff/DiffCalculator.java
@@ -1,10 +1,7 @@
 package io.github.yangziwen.checkstyle.diff;
 
 import java.io.File;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
@@ -16,7 +13,6 @@ import java.util.stream.Collectors;
 import org.apache.commons.collections.CollectionUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.Status;
-import org.eclipse.jgit.diff.ContentSource;
 import org.eclipse.jgit.diff.DiffAlgorithm;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.diff.Edit;
@@ -26,13 +22,6 @@ import org.eclipse.jgit.diff.RawTextComparator;
 import org.eclipse.jgit.diff.RenameDetector;
 import org.eclipse.jgit.dircache.DirCache;
 import org.eclipse.jgit.dircache.DirCacheIterator;
-import org.eclipse.jgit.errors.AmbiguousObjectException;
-import org.eclipse.jgit.errors.LargeObjectException;
-import org.eclipse.jgit.errors.MissingObjectException;
-import org.eclipse.jgit.lib.AbbreviatedObjectId;
-import org.eclipse.jgit.lib.AnyObjectId;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectLoader;
 import org.eclipse.jgit.lib.ObjectReader;
@@ -58,27 +47,13 @@ import lombok.Getter;
 @Builder
 public class DiffCalculator {
 
-    private static final int DEFAULT_BIG_FILE_THRESHOLD = 10 * 1024 * 1024;
-
-    private static final Field DIFF_ENTRY_OLD_ID_FIELD = getField(DiffEntry.class, "oldId");
-
-    private static final Field DIFF_ENTRY_NEW_ID_FIELD = getField(DiffEntry.class, "newId");
-
-    private static final Method DIFF_ENTRY_ADD_METHOD = getDiffEntryAddMethod();
-
-    private static final Method DIFF_ENTRY_MODIFY_METHOD = getDiffEntryModifyMethod();
-
-    private static final byte[] EMPTY = new byte[] {};
-
-    private static final byte[] BINARY = new byte[] {};
-
     private DiffAlgorithm diffAlgorithm;
 
     @Builder.Default
     private RawTextComparator comparator = RawTextComparator.DEFAULT;
 
     @Builder.Default
-    private int bigFileThreshold = DEFAULT_BIG_FILE_THRESHOLD;
+    private int bigFileThreshold = DiffHelper.DEFAULT_BIG_FILE_THRESHOLD;
 
     /**
      * calculate the diff between the old revision and the new revision
@@ -124,7 +99,8 @@ public class DiffCalculator {
                             RawText oldText = oldBytes != null ? new RawText(oldBytes) : RawText.EMPTY_TEXT;
                             RawText newText = new RawText(indexedFileContentMap.get(filePath));
                             DiffEntry entry = oldBytes == null
-                                    ? createAddDiffEntry(filePath, oldCommit) : createModifyDiffEntry(filePath);
+                                    ? DiffHelper.createAddDiffEntry(filePath, oldCommit)
+                                    : DiffHelper.createModifyDiffEntry(filePath);
                             return DiffEntryWrapper.builder()
                                     .gitDir(repoDir)
                                     .diffEntry(entry)
@@ -197,7 +173,7 @@ public class DiffCalculator {
 
     private RawText newRawText(DiffEntry entry, DiffEntry.Side side, ObjectReader reader) {
         try {
-            return new RawText(open(side, entry, reader));
+            return new RawText(DiffHelper.open(entry, side, reader, bigFileThreshold));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -210,114 +186,6 @@ public class DiffCalculator {
             editList.add(edit);
         }
         return editList;
-    }
-
-    private byte[] open(DiffEntry.Side side, DiffEntry entry, ObjectReader reader) throws Exception {
-        if (entry.getMode(side) == FileMode.GITLINK) {
-            return writeGitLinkText(entry.getId(side));
-        }
-        if (entry.getMode(side) == FileMode.MISSING) {
-            return EMPTY;
-        }
-        if (entry.getMode(side).getObjectType() != Constants.OBJ_BLOB) {
-            return EMPTY;
-        }
-        AbbreviatedObjectId id = entry.getId(side);
-        if (!id.isComplete()) {
-            Collection<ObjectId> ids = reader.resolve(id);
-            if (ids.size() == 1) {
-                id = AbbreviatedObjectId.fromObjectId(ids.iterator().next());
-                switch (side) {
-                    case OLD:
-                        DIFF_ENTRY_OLD_ID_FIELD.set(entry, id);
-                        break;
-                    case NEW:
-                        DIFF_ENTRY_NEW_ID_FIELD.set(entry, id);
-                        break;
-                    default:
-                        break;
-                }
-            }
-            else if (ids.size() == 0) {
-                throw new MissingObjectException(id, Constants.OBJ_BLOB);
-            }
-            else {
-                throw new AmbiguousObjectException(id, ids);
-            }
-        }
-        ContentSource cs = ContentSource.create(reader);
-        try {
-            ObjectLoader ldr = new ContentSource.Pair(cs, cs).open(side, entry);
-            return ldr.getBytes(bigFileThreshold);
-
-        } catch (LargeObjectException.ExceedsLimit overLimit) {
-            return BINARY;
-
-        } catch (LargeObjectException.ExceedsByteArrayLimit overLimit) {
-            return BINARY;
-
-        } catch (LargeObjectException.OutOfMemory tooBig) {
-            return BINARY;
-
-        } catch (LargeObjectException tooBig) {
-            tooBig.setObjectId(id.toObjectId());
-            throw tooBig;
-        }
-    }
-
-    private static byte[] writeGitLinkText(AbbreviatedObjectId id) {
-        if (ObjectId.zeroId().equals(id.toObjectId())) {
-            return EMPTY;
-        }
-        return Constants.encodeASCII("Subproject commit " + id.name() + "\n");
-    }
-
-    private static <T> Field getField(Class<T> clazz, String fieldName) {
-        try {
-            Field field = clazz.getDeclaredField(fieldName);
-            field.setAccessible(true);
-            return field;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static DiffEntry createAddDiffEntry(String path, AnyObjectId id) {
-        try {
-            return (DiffEntry) DIFF_ENTRY_ADD_METHOD.invoke(null, path, id);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static DiffEntry createModifyDiffEntry(String path) {
-        try {
-            return (DiffEntry) DIFF_ENTRY_MODIFY_METHOD.invoke(null, path);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static Method getDiffEntryAddMethod() {
-        Method method;
-        try {
-            method = DiffEntry.class.getDeclaredMethod("add", String.class, AnyObjectId.class);
-            method.setAccessible(true);
-            return method;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static Method getDiffEntryModifyMethod() {
-        Method method;
-        try {
-            method = DiffEntry.class.getDeclaredMethod("modify", String.class);
-            method.setAccessible(true);
-            return method;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 
 }
